ca5f45bcd6a361a08c7ac519baec3063bcec0fb5
Nikita Yeldynov <yeldynov@gmail.com>
2024-06-28 08:27:38 +0300

    to trigger deployment

--- components/Guest.tsx
+++ components/Guest.tsx
@@ -4,7 +4,7 @@ const Guest = () => {
   return (
     <div className='guest'>
       <h1>Welcome</h1>
-      <p>Please sign in to manage your transactions</p>
+      <p>Please login to manage your transactions</p>
       <SignInButton />
     </div>
   )
