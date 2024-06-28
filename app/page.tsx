3ba3d98c1f26d455867ce2ad81f5fbfedcce4ab3
Nikita Yeldynov <yeldynov@gmail.com>
2024-06-28 07:44:30 +0300

    + income & expense cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AddTransaction from '@/components/AddTransaction'
 import Balance from '@/components/Balance'
 import Guest from '@/components/Guest'
+import IncomeExpense from '@/components/IncomeExpense'
 import { currentUser } from '@clerk/nextjs/server'
 
 const HomePage = async () => {
@@ -14,6 +15,7 @@ const HomePage = async () => {
     <main>
       <h2>Welcome, {user.firstName} </h2>
       <Balance />
+      <IncomeExpense />
       <AddTransaction />
     </main>
   )
---
+++ components/IncomeExpense.tsx
@@ -0,0 +1,20 @@
+import getIncomeExpense from '@/app/actions/getIncomeExpense'
+
+const IncomeExpense = async () => {
+  const { income, expense } = await getIncomeExpense()
+
+  return (
+    <div className='inc-exp-container'>
+      <div>
+        <h4>Income</h4>
+        <p className='money plus'>${income}</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p className='money minus'>${expense}</p>
+      </div>
+    </div>
+  )
+}
+
+export default IncomeExpense
