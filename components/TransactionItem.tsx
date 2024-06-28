5fbf59e09c35090603396d4298afef9026e6b3a1
Nikita Yeldynov <yeldynov@gmail.com>
2024-06-28 08:09:03 +0300

    + delete transaction

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -1,8 +1,28 @@
+'use client'
+
+import deleteTransaction from '@/app/actions/deleteTransaction'
 import { addCommas } from '@/lib/utils'
 import { Transaction } from '@/types/Transaction'
+import { toast } from 'react-toastify'
 
 const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
   const sign = transaction.amount < 0 ? '-' : '+'
+
+  const handleDeleteTransaction = async (id: string) => {
+    const confirmed = window.confirm(
+      'Are you sure you want to delete this transaction?'
+    )
+
+    if (!confirmed) return
+
+    const { message, error } = await deleteTransaction(id)
+
+    if (error) {
+      toast.error(error)
+    }
+
+    toast.success(message)
+  }
 
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
@@ -10,6 +30,12 @@ const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
       <span>
         {sign}${addCommas(Math.abs(transaction.amount))}
       </span>
+      <button
+        onClick={() => handleDeleteTransaction(transaction.id)}
+        className='delete-btn'
+      >
+        x
+      </button>
     </li>
   )
 }
